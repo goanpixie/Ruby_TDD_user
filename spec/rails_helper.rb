737649ba7dcc187fb7a15e7db4dedd6af297f5bf
Priyanka <priyanka3007@gmail.com>
2017-02-09 00:15:14 -0800

    midway

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,23 +1,4 @@
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
-  config.include Capybara::DSL
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-end
-endCopy
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
